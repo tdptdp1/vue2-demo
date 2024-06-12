7b859705ecd6c25d4860eb7baf680e4747ac49ac
TDP <1240533759@qq.com>
2024-06-12 16:46:46 +0800

    店铺管理功能开发完成

--- src/views/AsideMenu/StoreMan.vue
+++ src/views/AsideMenu/StoreMan.vue
@@ -1,13 +1,94 @@
 <template>
-  <div></div>
+  <div id="storeMan">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>店铺管理</span>
+        </div>
+        <div  class="text item">
+          <div class="content">
+            <label for="">店铺名称</label>
+            <el-input v-model="input" placeholder="请输入内容"></el-input>
+          </div>
+          <div class="content top_conent">
+            <label for="">店铺公告</label>
+            <el-input
+              type="textarea"
+              :rows="5"
+              placeholder="请输入内容"
+              v-model="textarea">
+            </el-input>
+          </div>
+          <div class="content top_conent">
+            <label for="">店铺头像</label>
+            <div class="block">
+              <img src="../../assets/ths.jpg" alt="">
+            </div>
+          </div>
+          <div class="content top_conent">
+            <label for="">店铺图片</label>
+            <div class="block">
+              <img src="../../assets/ths.jpg" alt="">
+              <img src="../../assets/ths.jpg" alt="">
+            </div>
+          </div>
+          <div class="content top_conent">
+            <el-upload
+              action="https://jsonplaceholder.typicode.com/posts/"
+              list-type="picture-card"
+              :on-preview="handlePictureCardPreview"
+              :on-remove="handleRemove">
+              <i class="el-icon-plus"></i>
+            </el-upload>
+          </div>
+        </div>
+      </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'StoreMan'
+  name: 'StoreMan',
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+  #storeMan{
+    .box-card{
+      .text{
+        .content{
+          margin-bottom: 20px;
+          label{
+          margin-right: 15px;
+        }
+          .el-input,.el-textarea{
+            width: 280px;
+          }
+        }
+        .top_conent{
+          display: flex;
+          align-items: flex-start;
+          .block{
+            display: flex;
+            width: 100px;
+            height: 100px;
+            img{
+              margin-right: 10px;
+              width: 100%;
+              border-radius: 8px;
+            }
+          }
+          ::v-deep .el-upload{
+            width: 100px;
+            margin-left: 100px;
+            height: 100px;
+            line-height: 100px;
+          }
+        }
+      }
+    }
+  }
 </style>
--- src/views/GoodsChildren/GoodsGroup.vue
+++ src/views/GoodsChildren/GoodsGroup.vue
@@ -1,10 +1,94 @@
 <template>
-  <div id="GoodsGroup">GoodsGroup</div>
+  <div id="GoodsGroup">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>商品分类</span>
+        <el-button style="float: right; padding: 8px 8px" type="primary">添加分类</el-button>
+      </div>
+      <div  class="text item">
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            label="序号"
+            width="180">
+            <template slot-scope="scope">
+              <span style="margin-left: 10px">{{ scope.row.num }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="分类名称"
+            width="350">
+            <template slot-scope="scope">
+              <span style="margin-left: 10px">{{ scope.row.groutName }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="是否启用"
+            width="300">
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.isSelect"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template>
+              <el-button
+                size="mini"
+                >编辑</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="block">
+        <el-pagination
+          :current-page="currentPage4"
+          :page-sizes="[5, 8, 10, 14]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="14">
+        </el-pagination>
+      </div>
+
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'GoodsGroup'
+  name: 'GoodsGroup',
+  data () {
+    return {
+      tableData: [{
+        num: '1',
+        groutName: '热销榜',
+        isSelect: true
+      }, {
+        num: '2',
+        groutName: '单人精彩套餐',
+        isSelect: true
+      }, {
+        num: '3',
+        groutName: '冰爽饮品显示特惠',
+        isSelect: true
+      }, {
+        num: '4',
+        groutName: '精选热卖',
+        isSelect: true
+      }, {
+        num: '5',
+        groutName: '爽口凉菜',
+        isSelect: true
+      }]
+    }
+  }
 }
 </script>
 
